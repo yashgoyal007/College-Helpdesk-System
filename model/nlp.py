8e0609601d434c55ad31259b772cc70629550618
Yash Goyal <yashgoyal5815@gmail.com>
2026-08-13 12:44:42 +0530

    Fix NLTK startup

--- model/nlp.py
+++ model/nlp.py
@@ -1,4 +1,3 @@
-import os
 import re
 import nltk
 
@@ -6,27 +5,13 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 
-NLTK_DATA_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(__file__)),
-    "nltk_data"
-)
+# NLTK data is installed during the Render build
+# and stored in /opt/render/project/src/nltk_data
 
-os.makedirs(NLTK_DATA_DIR, exist_ok=True)
+NLTK_DATA_DIR = "/opt/render/project/src/nltk_data"
 
 if NLTK_DATA_DIR not in nltk.data.path:
     nltk.data.path.insert(0, NLTK_DATA_DIR)
-
-
-try:
-    nltk.data.find("tokenizers/punkt_tab")
-except LookupError:
-    nltk.download("punkt_tab", download_dir=NLTK_DATA_DIR)
-
-
-try:
-    nltk.data.find("corpora/wordnet")
-except LookupError:
-    nltk.download("wordnet", download_dir=NLTK_DATA_DIR)
 
 
 lemmatizer = WordNetLemmatizer()
