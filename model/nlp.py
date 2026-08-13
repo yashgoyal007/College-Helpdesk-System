18021c5d0df85290af4ee876cdaf1b6ce6fff745
Yash Goyal <yashgoyal5815@gmail.com>
2026-08-13 12:26:02 +0530

    Fix NLTK WordNet data path

--- model/nlp.py
+++ model/nlp.py
@@ -1,20 +1,32 @@
-
+import os
 import re
 import nltk
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
-# Download required NLTK resources
+
+# Use a fixed NLTK data folder inside the Render project
+NLTK_DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "nltk_data")
+
+os.makedirs(NLTK_DATA_DIR, exist_ok=True)
+
+if NLTK_DATA_DIR not in nltk.data.path:
+    nltk.data.path.insert(0, NLTK_DATA_DIR)
+
+
+# Check required NLTK resources
 try:
     nltk.data.find("tokenizers/punkt_tab")
 except LookupError:
-    nltk.download("punkt_tab")
+    nltk.download("punkt_tab", download_dir=NLTK_DATA_DIR)
+
 
 try:
     nltk.data.find("corpora/wordnet")
 except LookupError:
-    nltk.download("wordnet")
+    nltk.download("wordnet", download_dir=NLTK_DATA_DIR)
+
 
 lemmatizer = WordNetLemmatizer()
 
@@ -33,4 +45,3 @@ def clean_text(text):
     ]
 
     return words
-
